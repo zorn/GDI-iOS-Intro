8708c551478f8502ea8930d33049eb5eb0a38668
Mike Zornek <mike@clickablebliss.com>
2015-01-30 21:10:44 -0500

    add functions

---
+++ block-02/Functions.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+func divisionDescription(num: Double, den: Double) {
+    println("\(num) divided by \(den) equals \(num / den)")
+}
+
+divisionDescription(9, 3)
