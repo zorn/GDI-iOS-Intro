abe82247c67bdee926374c65367d90ff683316b7
Michael Zornek <mike@clickablebliss.com>
2015-01-31 22:03:11 -0500

    More swift playground samples.

--- block-02-03/06-IfElseAndOptionals.playground/section-1.swift
+++ block-02-03/06-IfElseAndOptionals.playground/section-1.swift
@@ -1,5 +1,58 @@
-// Playground - noun: a place where people can play
-
 import Cocoa
 
-var str = "Hello, playground"
+// Many times in programming you'll want to perform different code depending on state. These are called control structures. 
+
+// if/else
+let a = 45
+if (a > 100) {
+    println("big number")
+} else {
+    println("small number")
+}
+
+// if elseif ... else
+let message = "Hello world!"
+let lengthOfMessage = countElements(message)
+if (lengthOfMessage > 10) {
+    println("not short")
+} else if (lengthOfMessage > 5) {
+    println("kinda short")
+} else {
+    println("very short")
+}
+
+// optionals -- help represent values that may be nil
+let people = ["Mike", "Josh", "Steve"]
+let mikeIndex = find(people, "Mike")
+
+if mikeIndex != nil {
+    let foundIndex = mikeIndex!
+    println(foundIndex)
+}
+
+let possibleIndex = find(people, "Josh")
+if let foundIndex = possibleIndex {
+    println("Found an index for Josh at: \(foundIndex)")
+}
+
+if let foundIndex = find(people, "Josh") {
+    println("Found an index for Josh at: \(foundIndex)")
+}
+
+// switch -- very powerful, can match values, ranges, enums, tupals
+
+var statusCode: Int = 404
+var errorString: String
+switch statusCode {
+    case 400:
+        errorString = "Bad request."
+    case 401:
+        errorString = "Unauthorized."
+    case 403:
+        errorString = "Forbidden."
+    case 404:
+        errorString = "Not found."
+    default:
+        errorString = "None."
+}
+println(errorString)
--- block-02-03/07-Structs.playground/section-1.swift
+++ block-02-03/07-Structs.playground/section-1.swift
@@ -1,5 +1,24 @@
-// Playground - noun: a place where people can play
-
 import Cocoa
 
-var str = "Hello, playground"
+struct Vertex {
+    var x: Double
+    var y: Double
+    
+    func moveByX(deltaX: Double) -> Vertex {
+        return Vertex(x: x + deltaX, y: y)
+    }
+}
+
+struct Size {
+    var width, height: Double
+}
+
+struct Rectangle {
+    var size: Size
+    var topLeftCorner: Vertex
+}
+
+var point = Vertex(x: 3.0, y: 4.0)
+point = point.moveByX(3)
+
+var rectangle = Rectangle(size: Size(width: 100.0, height: 200.0), topLeftCorner: point)
