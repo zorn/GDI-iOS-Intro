15021663c8271d9e8632950e1b2c95463ca4f682
Mike Zornek <mike@clickablebliss.com>
2015-02-02 22:06:57 -0500

    Adding phase2

---
+++ block-06/ToDoApp-phase1/ToDoApp/ToDoListViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class ToDoListViewController : UIViewController, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK: - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Basic", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = "IndexPath: \(indexPath.row)"
+        return cell
+    }
+
+}
+
--- block-06/ToDoApp-phase2/ToDoApp/ToDoListViewController.swift
+++ block-06/ToDoApp-phase2/ToDoApp/ToDoListViewController.swift
@@ -1,21 +1,33 @@
 import UIKit
 
 class ToDoListViewController : UIViewController, UITableViewDataSource {
-
+    
+    var items: [ToDoItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.prepareDataStore()
     }
     
-    //MARK: UITableViewDataSource
+    //MARK: - Private
+    
+    func prepareDataStore() {
+        for i in 0...100 {
+            let newItem = ToDoItem(name: "To Do Item \(i)", isDone: false)
+            items.append(newItem)
+        }
+    }
+
+    //MARK: - UITableViewDataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return items.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let item = self.items[indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier("Basic", forIndexPath: indexPath) as UITableViewCell
-        cell.textLabel.text = "IndexPath: \(indexPath.row)"
+        cell.textLabel?.text = item.name
         return cell
     }
 
